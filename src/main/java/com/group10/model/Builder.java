5a106c476dd7748bb7ba855e020db434a067fb15
Gea1926 <l.lanzetta6@studenti.unisa.it>
2026-05-30 18:41:55 +0200

    Interfaccia Builder.java

---
+++ src/main/java/com/group10/model/Builder.java
@@ -0,0 +1,16 @@
+package com.group10.model;
+
+/**
+ * Interfaccia base per il pattern Builder (Task T1.3).
+ * Utilizza i Generics <T> in modo che possa essere usata 
+ * sia per TrackBuilder che per PlaylistBuilder.
+ */
+public interface Builder<T> {
+    
+    /**
+     * Metodo finale che restituisce l'oggetto costruito.
+     * @return L'istanza dell'oggetto costruito e validato.
+     */
+    T build();
+    
+}
