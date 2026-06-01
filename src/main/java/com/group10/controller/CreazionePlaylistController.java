58616a99a55486f2431d2958b957a011f2f61e67
VxVilla55 <alfonsovillani1242.av@gmail.com>
2026-06-01 18:08:36 +0200

    update a CreazionePlayistController
    - ora è il controller che crea la sua View come per gli altri controller

--- src/main/java/com/group10/controller/CreazionePlaylistController.java
+++ src/main/java/com/group10/controller/CreazionePlaylistController.java
@@ -1,7 +1,10 @@
 package com.group10.controller;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 
 public class CreazionePlaylistController {
@@ -9,6 +12,29 @@ public class CreazionePlaylistController {
     // Questo si collega alla casella di testo creata in Scene Builder
     @FXML
     private TextField playlistNameInput;
+    
+    private final String viewPath  = "/com/group10/view/CreazionePlaylist.fxml";
+    private Parent view = null;
+    
+    public CreazionePlaylistController() {
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource(viewPath)
+        );
+        
+        //loader.setRoot(this);
+        loader.setController(this);
+        
+        try {
+            //carica effettivamente la grafica FXML
+            view = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException("Path della view errato: " + viewPath);
+        }
+    }
+
+    public Parent getView() {
+        return view;
+    }
 
     // Questo metodo scatterà quando l'utente cliccherà il bottone "Crea"
     @FXML
