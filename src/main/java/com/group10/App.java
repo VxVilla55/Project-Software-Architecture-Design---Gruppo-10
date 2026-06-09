a66618fa9787efcb4c542509378cf1095c51f32d
Nicola <n.liguori1@studenti.unisa.it>
2026-06-09 11:51:15 +0200

    Fix HomeBtn
    Aggiunta di uno stile css, collegamento in app.java

--- src/main/java/com/group10/App.java
+++ src/main/java/com/group10/App.java
@@ -25,28 +25,27 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        MusicCatalogue.getInstance().load();
-        stage.setOnCloseRequest(event -> {
+public void start(Stage stage) throws IOException {
+    MusicCatalogue.getInstance().load();
+    stage.setOnCloseRequest(event -> {
         com.group10.model.state.PlaybackEngine.getInstance().stopSimulation();
         javafx.application.Platform.exit();
         System.exit(0);
     });
-  
-        // -- ESEGUITE UNA SOLA VOLTA per riempire il programma inzialmente
-        // -- Altrimenti dovete cancellare il catalogue.json nella cartella "data"
-        // fillInstance();
-        
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/group10/view/MainView.fxml"));
-        MainViewController controller = MainViewController.getInstance();
-        loader.setController(controller);
-        
-        stage.setScene(new Scene(loader.load()));
-        stage.setMinWidth(1300); 
-        stage.setMinHeight(800);
-        stage.setTitle("MyMusicPlayer");
-        stage.show();        
-    }
+
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/group10/view/MainView.fxml"));
+    MainViewController controller = MainViewController.getInstance();
+    loader.setController(controller);
+
+    Scene scena = new Scene(loader.load());
+    scena.getStylesheets().add(getClass().getResource("/com/group10/view/styles.css").toExternalForm());
+    stage.setScene(scena);
+    
+    stage.setMinWidth(1300); 
+    stage.setMinHeight(800);
+    stage.setTitle("MyMusicPlayer");
+    stage.show();        
+}
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
