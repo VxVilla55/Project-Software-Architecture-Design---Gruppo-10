f81e1ef666462fbbd6064aa36055a99a0ca13836
itsjhell <99731715+itsjhell@users.noreply.github.com>
2026-06-10 14:47:31 +0200

    Create RepeatTrack.java

---
+++ src/main/java/com/group10/model/strategy/RepeatTrack.java
@@ -0,0 +1,21 @@
+package com.group10.model.strategy;
+
+import com.group10.model.state.PlaybackEngine;
+
+/**
+ *
+ * @author group10
+ *
+ * Strategy concreta con ripetizione della singola traccia: a fine
+ * traccia la riproduce di nuovo dall'inizio
+ *
+ * PATTERN: ConcreteStrategy di RepeatMode
+ */
+public class RepeatTrack implements PlaybackMode {
+
+    // riproduce di nuovo la traccia corrente dall'inizio
+    @Override
+    public void onTrackEnd(PlaybackEngine engine) {
+        engine.replayCurrent();
+    }
+}
