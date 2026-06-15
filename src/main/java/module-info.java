2f6776d32a8ca9f14ebbe30ad38b9a60baded5e9
itsjhell <99731715+itsjhell@users.noreply.github.com>
2026-06-15 12:49:33 +0200

    persistenza tags

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,12 +5,13 @@ module com.group10 {
     requires javafx.graphics;
     requires com.google.gson;
 
-    
     opens com.group10.controller to javafx.fxml;
     opens com.group10.controller.track to javafx.fxml;
     opens com.group10.controller.playlist to javafx.fxml;
+
+    opens com.group10.model to com.google.gson;
     opens com.group10.model.persistence to com.google.gson;
-    
+
     exports com.group10;
     exports com.group10.controller;
 }
